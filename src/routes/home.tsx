1a62e6a1e73b2cf958bb53d437a80a31c3d3205c
khalidibnwalid <kh.hawajreh@gmail.com>
2025-03-17 23:52:57 +0300

    install  & init shadcn

---
+++ src/routes/home.tsx
@@ -0,0 +1,15 @@
+import type { Route } from ".react-router/types/app/+types/root";
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <>
+    </>
+  );
+}
