a23d0e3a530699241363fa9a62daf17cfdc70f5f
khalidibnwalid <kh.hawajreh@gmail.com>
2025-03-18 18:19:39 +0300

    feat(server): add the App struct - implement App struct for the server - add .Use(middleware) method for App

---
+++ server/core/app.go
@@ -0,0 +1,44 @@
+package core
+
+import (
+	"net/http"
+)
+
+type Middleware func(http.Handler) http.Handler
+
+type App struct {
+	Mux         *http.ServeMux
+	middlewares []Middleware
+}
+
+func NewApp() *App {
+	return &App{
+		Mux:         http.NewServeMux(),
+		middlewares: make([]Middleware, 0),
+	}
+}
+
+func (s *App) Use(mw ...Middleware) {
+	s.middlewares = append(s.middlewares, mw...)
+}
+
+func (s *App) HandleFunc(pattern string, handler http.HandlerFunc) {
+	lastMiddleware := len(s.middlewares) - 1
+
+	if lastMiddleware == -1 {
+		s.Mux.Handle(pattern, handler)
+		return
+	}
+
+	for i := len(s.middlewares) - 1; i >= 0; i-- {
+		md := s.middlewares[i]
+		// encapsulating handlers
+		if h, ok := md(handler).(http.HandlerFunc); ok {
+			handler = h
+		} else {
+			panic("middleware should return http.HandlerFunc")
+		}
+	}
+
+	s.Mux.HandleFunc(pattern, handler)
+}
--- server/main.go
+++ server/main.go
@@ -3,11 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/khalidibnwalid/Luma/core"
 )
 
+func TestMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("1 Start")
+		next.ServeHTTP(w, r)
+		fmt.Println("1 done")
+	})
+}
+
+func Test2Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("2 Start")
+		next.ServeHTTP(w, r)
+		fmt.Println("2 done")
+	})
+}
+
 func main() {
+	app := core.NewApp()
+	app.Use(TestMiddleware)
+    app.Use(Test2Middleware)
+	app.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "hello\n")
+	})
 	v1 := http.NewServeMux()
-	v1.Handle("/v1/", http.StripPrefix("/v1", routes()))
+	v1.Handle("/v1/", http.StripPrefix("/v1", app.Mux))
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -15,13 +39,14 @@ func main() {
 	}
 
 	server.ListenAndServe()
+	// app.ha
 }
 
-func routes() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "hello\n")
-	})
+// func routes() *http.ServeMux {
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+// 		fmt.Fprintf(w, "hello\n")
+// 	})
 
-	return mux
-}
+// 	return mux
+// }
