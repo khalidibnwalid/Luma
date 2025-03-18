e2bfeb8995db9b6fec8c558dea96dd1dbd3a59d4
khalidibnwalid <kh.hawajreh@gmail.com>
2025-03-18 18:38:35 +0300

    feat(server): add Logging middleware

--- server/main.go
+++ server/main.go
@@ -5,28 +5,12 @@ import (
 	"net/http"
 
 	"github.com/khalidibnwalid/Luma/core"
+	"github.com/khalidibnwalid/Luma/server/middlewares"
 )
-
-func TestMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("1 Start")
-		next.ServeHTTP(w, r)
-		fmt.Println("1 done")
-	})
-}
-
-func Test2Middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("2 Start")
-		next.ServeHTTP(w, r)
-		fmt.Println("2 done")
-	})
-}
 
 func main() {
 	app := core.NewApp()
-	app.Use(TestMiddleware)
-    app.Use(Test2Middleware)
+	app.Use(middlewares.Logging)
 	app.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "hello\n")
 	})
