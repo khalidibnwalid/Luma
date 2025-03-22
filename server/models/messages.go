68394b6f18756501f9c53d27d42a2b236936645a
khalidibnwalid <kh.hawajreh@gmail.com>
2025-03-22 13:40:22 +0300

    feat(messages): create basic Message model

---
+++ server/models/messages.go
@@ -0,0 +1,63 @@
+package models
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type Message struct {
+	ID        bson.ObjectID `bson:"_id" json:"id"`
+	UserID    string        `bson:"user_id"`
+	RoomID    string        `bson:"room_id"`
+	Message   string        `bson:"message"`
+	CreatedAt int64         `bson:"created_at"`
+	UpdatedAt int64         `bson:"updated_at"`
+}
+
+func (msg *Message) Create(db *mongo.Database) error {
+	msg.ID = bson.NewObjectID()
+	msg.CreatedAt = time.Now().Unix()
+	msg.UpdatedAt = time.Now().Unix()
+
+	coll := db.Collection("messages")
+	if _, err := coll.InsertOne(context.TODO(), msg); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (msg *Message) GetAllMessages(db *mongo.Database, channelID string) ([]Message, error) {
+	coll := db.Collection("messages")
+	cursor, err := coll.Find(context.TODO(), bson.M{"channel_id": channelID})
+	if err != nil {
+		return nil, err
+	}
+
+	var messages []Message
+	if err := cursor.All(context.Background(), &messages); err != nil {
+		return nil, err
+	}
+
+	return messages, nil
+}
+
+func (msg *Message) Update(db *mongo.Database) error {
+	msg.UpdatedAt = time.Now().Unix()
+	coll := db.Collection("messages")
+	if _, err := coll.UpdateOne(context.TODO(), bson.M{"_id": msg.ID}, bson.M{"$set": msg}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (msg *Message) Delete(db *mongo.Database) error {
+	coll := db.Collection("messages")
+	if _, err := coll.DeleteOne(context.TODO(), bson.M{"_id": msg.ID}); err != nil {
+		return err
+	}
+	return nil
+}
